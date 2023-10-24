ca9e1c4cd75afd2c5a419793769cc864c8bcf0e1
fujita ryouhei <fujita.dora12.chinamon@gmail.com>
2023-10-24 10:39:54 +0900

    ビューの追加

---
+++ src/resources/views/thanks.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="sanitize.css">
+    <link rel="stylesheet" href="thanks.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="thanks__content">
+        <div class="thanks__heading">
+            <h2>ご意見いただきありがとうございました</h2>
+        </div>
+    </div>
+    <form action="" class="form">
+        <div class="form__button">
+            <button class="form__button-submit" type="submit">トップページへ</button>
+        </div>
+    </form>
+</body>
+</html>
